f0e3405e0fdeba1e448bb460e7801e2f57b1a4ad
Crowdbotics <team@crowdbotics.com>
2022-05-18 11:47:08 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "my24x7voip_35265",
+  url: "https://my24x7voip_35265.botics.co",
+  api: "https://my24x7voip_35265.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
